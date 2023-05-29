951213166360377ab8b097fcdec7675afe46dbd9
ShirshakShahi <royalshirshal000@gmail.com>
2023-05-29 22:28:50 +0545

    dummy meals pushed to firebase

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Food Ordering App with Max.
+### So, if we do, response.json() when GET it returns the json value into object? and when we do body:JSON.stringify(obj) , it sets object into json file? is it true?
+
+Yes, that's correct!
+
+When you make a GET request and use response.json(), it parses the response body assuming it is in JSON format and returns the corresponding JavaScript object. This allows you to easily access and work with the data retrieved from the server.
+
+On the other hand, when you want to send data to the server, such as in a POST or PUT request, you often need to convert the JavaScript object into a JSON string. You can use JSON.stringify(obj) to achieve this. It takes an object as input and returns a JSON string representation of that object. This JSON string can then be included in the request body, typically as part of the body property when using the Fetch API or other similar methods.
+
+`So, response.json() converts a JSON response into a JavaScript object, and JSON.stringify(obj) converts a JavaScript object into a JSON string.`
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,42 +1,39 @@
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+
 
 const AvailableMeals = () => {
 
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://react-meals-f2db4-default-rtdb.firebaseio.com/meals.json');
+            const data = await response.json();
+            const DUMMY_MEALS = [];
+            for (const key in data) {
+                DUMMY_MEALS.push({
+                    id: key,
+                    description: data[key].description,
+                    name: data[key].name,
+                    price: data[key].price
+                });
+            }
+            setMeals(DUMMY_MEALS);
+        }
+        fetchData();
+    }, []);
+
+
+    const mealsList = meals.map(meal => <MealItem
         id={meal.id}
         key={meal.id}
         name={meal.name}
         description={meal.description}
         price={meal.price}
-    />)
+    />);
 
     return <Card className={classes.meals}>
         <ul>
